6e67dd4cb63757db153c5a9a6369af18e5a463fc
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-11-03 13:49:18 -0700

    apply ktlin

--- app/src/test/java/cmpt362/group14/gostudent/ExampleUnitTest.kt
+++ app/src/test/java/cmpt362/group14/gostudent/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package cmpt362.group14.gostudent
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
