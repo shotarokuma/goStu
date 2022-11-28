628bebab7c6c4c9787b5dd4537d1470f438fe51d
yatik17 <yatikmittal@gmail.com>
2022-11-27 23:41:04 -0800

    Marketplace UI created

---
+++ app/src/main/java/cmpt362/group14/gostudent/MarketplaceActivity.kt
@@ -0,0 +1,11 @@
+package cmpt362.group14.gostudent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MarketplaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marketplace)
+    }
+}
