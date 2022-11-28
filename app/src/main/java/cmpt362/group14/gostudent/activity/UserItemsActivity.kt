73de5032b7a9fa24caf3e5c840bad5cb5ed6ed0c
jaspreetsingh <jerryjagdev@gmail.com>
2022-11-27 23:50:42 -0800

    added activities for edit profile and useritems

---
+++ app/src/main/java/cmpt362/group14/gostudent/activity/UserItemsActivity.kt
@@ -0,0 +1,12 @@
+package cmpt362.group14.gostudent.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cmpt362.group14.gostudent.R
+
+class UserItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_items)
+    }
+}
