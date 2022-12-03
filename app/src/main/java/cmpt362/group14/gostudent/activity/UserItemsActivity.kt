cf41744e501c4690b7a0a28836dcbc755b2f8574
jaspreetsingh <jerryjagdev@gmail.com>
2022-12-02 20:14:43 -0800

    item_profile_settings

---
+++ app/src/main/java/cmpt362/group14/gostudent/activity/UserItemsActivity.kt
@@ -0,0 +1,62 @@
+package cmpt362.group14.gostudent.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import cmpt362.group14.gostudent.MarketplaceAdapter
+import cmpt362.group14.gostudent.R
+import cmpt362.group14.gostudent.databinding.ActivityMarketplaceBinding
+import cmpt362.group14.gostudent.model.Item
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+
+class UserItemsActivity : AppCompatActivity() {
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var binding: ActivityMarketplaceBinding
+    private var itemList = ArrayList<Item>()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var uid: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMarketplaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addItemBtn.setOnClickListener() {
+            val intent = Intent(this, AddItemActivity::class.java)
+            startActivity(intent)
+        }
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        uid = auth.currentUser?.uid.toString()
+        fetchItems()
+    }
+
+    private fun fetchItems() {
+        db.collection("item")
+            .orderBy("createdTime")
+            .whereEqualTo("sellerId",uid)
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    Log.w(HomeChatActivity.TAG, "Listen failed.", error)
+                    return@addSnapshotListener
+                }
+
+                for (dc in value!!.documentChanges) {
+                    when (dc.type) {
+                        DocumentChange.Type.ADDED -> {
+                            val item: Item = dc.document.toObject(Item::class.java)
+                            itemList.add(item)
+                            binding.listviewItems.adapter = MarketplaceAdapter(this, itemList)
+                        }
+                        DocumentChange.Type.MODIFIED -> TODO("Not yet implemented")
+                        DocumentChange.Type.REMOVED -> TODO("Not yet implemented")
+                    }
+                }
+            }
+    }
+}
